353cac801131220843b29faddfc0420d7909d737
Johongirr <johongir_rahimov@bk.ru>
2020-05-23 15:44:55 +0200

    Updates

---
+++ precache-manifest.ac6379e75109640e4695427acc65952b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e75b68d52486974da0ae19556b7c357a",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "e3ef36683aa3fd92c0b1",
+    "url": "/react-todo-app/static/css/main.892e66f5.chunk.css"
+  },
+  {
+    "revision": "3b9b648b868e0365a018",
+    "url": "/react-todo-app/static/js/2.4b59636d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.4b59636d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3ef36683aa3fd92c0b1",
+    "url": "/react-todo-app/static/js/main.def6c494.chunk.js"
+  },
+  {
+    "revision": "990cf42d1961046a6e6a",
+    "url": "/react-todo-app/static/js/runtime-main.7f47a608.js"
+  }
+]);
